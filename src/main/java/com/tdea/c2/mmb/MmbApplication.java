a3a3ed5bf7382d4560200993dc4d0e9843afa79c
Rodrigo Ardila <rodrigo.ardila@correo.tdea.edu.co>
2026-03-18 21:10:27 -0500

    Subida de init del proyecto

---
+++ src/main/java/com/tdea/c2/mmb/MmbApplication.java
@@ -0,0 +1,13 @@
+package com.tdea.c2.mmb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmbApplication.class, args);
+	}
+
+}
