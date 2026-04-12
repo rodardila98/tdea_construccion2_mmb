3917b3f260a2e74506d76580f96dbe42c8cbd3d8
Pao Velez <paola.velez@correo.tdea.edu.co>
2026-04-12 15:20:58 -0500

    Creando @PatchMapping clase TecnicoController para modificación de estado

--- src/main/java/com/tdea/c2/mmb/controller/TecnicoController.java
+++ src/main/java/com/tdea/c2/mmb/controller/TecnicoController.java
@@ -1,6 +1,7 @@
 package com.tdea.c2.mmb.controller;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -83,4 +85,17 @@ public class TecnicoController {
 		tecnicoRepository.deleteById(id);
 		return ResponseEntity.noContent().build();
 } 
+	
+	@PatchMapping("/tecnicos/{id}")
+	public ResponseEntity<Tecnico> updateEstadoTec(@PathVariable("id") Integer id, @RequestBody Map<String, Object> cambios) {
+		Tecnico existente = tecnicoRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("No encontrado"));
+
+		if (cambios.containsKey("estadoTecnico")) {
+        existente.setEstadoTecnico((String) cambios.get("estadoTecnico"));
+    }
+
+    return ResponseEntity.ok(tecnicoRepository.save(existente));
+
+	}
 	}
