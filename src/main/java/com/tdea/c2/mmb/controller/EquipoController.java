c68ed91888d3a0b9c56f4614f6f23968d1824b77
Pao Velez <paola.velez@correo.tdea.edu.co>
2026-04-12 13:12:49 -0500

    Creando @DeleteMapping clase EquipoController

--- src/main/java/com/tdea/c2/mmb/controller/EquipoController.java
+++ src/main/java/com/tdea/c2/mmb/controller/EquipoController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -71,7 +72,15 @@ public class EquipoController {
 		existente.setModelo(equipo.getModelo());
 		existente.setTipo(equipo.getTipo());
 		return ResponseEntity.ok(equipoRepository.save(existente));
-}
+	}
+	
+	@DeleteMapping("/equipos/{id}")
+	public ResponseEntity<Equipo> deleteEquipo(@PathVariable("id")Long id){
+		equipoRepository.deleteById(id);
+		return ResponseEntity.noContent().build();
+		
+		
+	}
 }
 
 
