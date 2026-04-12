14618f13cae5da95f2cec769b13b9f39f584bc85
Pao Velez <paola.velez@correo.tdea.edu.co>
2026-04-12 13:24:31 -0500

    Creando @DeleteMapping clase UsuarioController

--- src/main/java/com/tdea/c2/mmb/controller/UsuarioController.java
+++ src/main/java/com/tdea/c2/mmb/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -85,5 +86,9 @@ public class UsuarioController {
 		return ResponseEntity.ok(usuarioRepository.save(existente));
 	}
 		
-		
+	@DeleteMapping("/usuarios/{id}")
+	public ResponseEntity<Usuario> deleteUsuario(@PathVariable("id")Integer id){
+		usuarioRepository.deleteById(id);
+		return ResponseEntity.noContent().build();
+}
 }
