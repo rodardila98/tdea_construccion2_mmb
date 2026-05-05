04e212947ace1306838864c727268c8ed23989c8
Paola Vélez <paola.velez@correo.tdea.edu.co>
2026-05-04 20:55:28 -0500

    Definición contrato IUsuarioService

--- src/main/java/com/tdea/c2/mmb/service/IUsuarioService.java
+++ src/main/java/com/tdea/c2/mmb/service/IUsuarioService.java
@@ -1,5 +1,15 @@
 package com.tdea.c2.mmb.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.tdea.c2.mmb.modelo.Usuario;
+
 public interface IUsuarioService {
 
+	List<Usuario> getAllUsuarios();
+	Optional<Usuario> getUsuarioById(Integer id);
+	Usuario createUsuario(Usuario usuario);
+	Usuario updateUsuario(Integer id, Usuario usuario);
+	void deleteUsuario(Integer id);
 }
